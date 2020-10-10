930d11e6af837632fe42ff8d53dada446b490a85
Kat Chonka <katchonka@gmail.com>
2020-10-10 18:48:11 -0400

    Home page modifications

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import world from './images/world.jpg';
+import React, {Component} from 'react';
+import globe from './images/globe.png';
+import FadeIn from 'react-fade-in';
+import AnimateOnChange from 'react-animation';
 
 function App() {
   const logoStyle = {
     font: "Ariel",
-    fontSize: 65,
-    color: "#27C777",
+    fontSize: 70,
+    color: "#424FFC",
     textAlign: "left",
     paddingLeft: "40px",
     paddingTop: "20px",
     margin: 0
   }
 
-  return (
-    <div style={{ backgroundImage: `url(${world})`,backgroundRepeat:'no-repeat',
-    backgroundSize:'cover', backgroundPosition: 'center', height: '100vh', margin: 0}}>
-      <h1 style={logoStyle}>Enlex</h1>
-    </div>
+  const backgroundImageStyle = {
+    backgroundImage: `url(${globe})`,
+    backgroundRepeat:'no-repeat',
+    backgroundSize:'cover',
+    backgroundPosition: 'center',
+    height: '100vh',
+    margin: 0,
+  }
 
+  const textStyle = {
+    //font: "Ariel",
+    fontSize: 26,
+    color: "#424FFC",
+    textAlign: "center",
+  }
+
+  return (
+    <div style={backgroundImageStyle}>
+      <h1 style={logoStyle}>Enlex</h1>
+
+      <FadeIn>
+        <h2 style={textStyle}> The flashcard learning program that exposes you to </h2>
+        <h2 style={textStyle}> new vocabulary in any target language! </h2>
+      </FadeIn>
+
+      <span>Languages</span>
+    </div>
   );
+
+
 }
+
+
 
 export default App;
