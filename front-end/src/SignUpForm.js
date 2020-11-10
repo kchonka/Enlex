88d0dcfbfe3c4bab06e1ea8141b55d2443d51377
Kat Chonka <katchonka@gmail.com>
2020-11-09 22:18:31 -0500

    Add basic signup form

--- front-end/src/SignUpForm.js
+++ front-end/src/SignUpForm.js
@@ -1,20 +1,34 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
-
-const signupstyle = {
-  color: "white"
-}
+import "./signupform.css"
 
 const SignUpForm = (user) => {
   return(
-    <div style={signupstyle}>
+    <div className="signUpBox">
       <h1>Sign Up</h1>
-      <form style={signupstyle}>
-        <TextField>
-          name="username"
-          floatingLabelText= "user name"
-          value={user.username}
-        </TextField>
+      <form>
+        <TextField
+          label="First name"
+          value={user.firstname}
+        />
+        <br/>
+        <br/>
+        <TextField
+          label="Last name"
+          value={user.lastname}
+        />
+        <br/>
+        <br/>
+        <TextField
+          label="Email"
+          value={user.email}
+        />
+        <br/>
+        <br/>
+        <TextField
+          label="Password"
+          value={user.password}
+        />
       </form>
     </div>
   );
