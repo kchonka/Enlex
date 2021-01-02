796ce01b549bc02c34f1906da1057bab1f3e4ed5
Kat Chonka <katchonka@gmail.com>
2021-01-02 18:23:01 -0500

    Change password hashing to use bcrypt

--- back-end/connection.py
+++ back-end/connection.py
@@ -1,8 +1,8 @@
 # Database instantiation class
 import mysql.connector
-import hashlib
 import os
-import binascii
+import bcrypt
+from errors import *
 
 class Connection:
     def __init__(self, host, user, password, db):
@@ -17,36 +17,36 @@ class Connection:
         self.cursor = self.cnx.cursor()
         self.cnx._open_connection() # doesn't work without this part; necessary
 
+
     # Add a user to the User database
     def addUser(self, first_name, last_name, email, entered_pass):
-        salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
-        hashed_pass = hashlib.pbkdf2_hmac('sha512', entered_pass.encode('utf-8'), salt, 100000)
-        hashed_pass = binascii.hexlify(hashed_pass)
-        #hashed_pass = (salt + hashed_pass).decode('ascii')
-        #hashed_pass = salt + hashed_pass
-
+        entered_pass = entered_pass.encode("utf-8")
+        salt = bcrypt.gensalt()
+        hashed_pass = bcrypt.hashpw(entered_pass, salt).decode('utf-8')
+        # Decode from bytes to string:
+        salt = salt.decode('utf-8')
+        # Store in the database
         add_user = "INSERT INTO users(first_name, last_name, email, salt, hashed_pass) VALUES (%s, %s, %s, %s, %s)"
         user_data = (first_name, last_name, email, salt, hashed_pass)
         self.cursor.execute(add_user, user_data)
 
 
-    # Verify that an entered_pass matches the one in the database
     def verifyPassword(self, email, entered_pass):
-        # Get the stored password for that email
-        get_user = "SELECT hashed_pass FROM users WHERE email = %s"
+        # Get the stored password & salt for the entered email:
+        get_user = "SELECT salt, hashed_pass FROM users WHERE email = %s"
         self.cursor.execute(get_user, (email,)) # trailing comma is necessary
-
         # Extract the contents of the query
         stored_pass = None
+        salt = None
         for values in self.cursor:
-            (stored_pass,) = values
-        salt = stored_pass[:64]
+            (salt, stored_pass) = values
+        # Hash the entered_pass and compare
+        entered_pass = entered_pass.encode("utf-8")
+        salt = salt.encode("utf-8")
+        hashed_pass = bcrypt.hashpw(entered_pass, salt).decode("utf-8")
+        # Return the result of the comparison (True or false)
+        return hashed_pass == stored_pass
 
-        # Hash the entered pass & see if it matches
-        entered_hash = hashlib.pbkdf2_hmac('sha512', entered_pass.encode('utf-8'), salt.encode('ascii'), 100000)
-        entered_hash = binascii.hexlify(entered_hash).decode('ascii')
-        entered_hash = salt + entered_hash
-        return salt+entered_hash == salt+stored_pass
 
     # Close the connection to the database
     def closeConnection(self):
