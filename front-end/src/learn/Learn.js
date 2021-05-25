afa23669822988514480e9afd7f62a3f8d598c34
Kat Chonka <katchonka@gmail.com>
2021-05-25 16:49:18 -0400

    Add preliminary function to query for news sources

--- front-end/src/learn/Learn.js
+++ front-end/src/learn/Learn.js
@@ -4,9 +4,6 @@ import SideBar from './SideBar';
 import { Dropdown } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css'; //for the Dropdown component
 import './learn.css';
-import Card from 'react-bootstrap/Card';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-
 
 const languageOptions = [
     { key: 'Arabic', text: 'Arabic', value: 'Arabic' },
@@ -26,19 +23,34 @@ export class Learn extends React.Component{
         super(props);
         this.state = { targetLanguage: 'English' }  // default to English
         this.handleLanguageChange = this.handleLanguageChange.bind(this);
+        this.getArticles = this.getArticles.bind(this);
     }
 
     handleLanguageChange = (e, { value }) => { this.setState({ targetLanguage: value }); }
+
+    // TODO: need function that maps languages to the written english equivalent, or diff state var
+    // Review React component life cycle
+    getArticles(){
+        const url = "https://newsapi.org/v2/sources?apiKey=" + apiKey;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => console.log(data["sources"][3]));
+
+    }
+
+    componentDidMount() {
+        this.getArticles();
+      }
 
     render(){
         return(
             <div id="learnPage">
                 <SideBar/>
                 <div id="main">
-                    <   div id="info">
+                    <div id="info">
                         <div>
                             <h2 style={{fontWeight: "bold", margin: "5"}}> Exploring News in Foreign Languages</h2>
-                            <h5 style={{margin: "0"}}> Select a Target Language. Showing results for: {this.state.targetLanguage} </h5>
+                            <h5 style={{margin: "0"}}> Showing results for: {this.state.targetLanguage} </h5>
                             <br/>
                         </div>
                         <div id="dropdownMenu">
@@ -51,7 +63,6 @@ export class Learn extends React.Component{
                             />
                         </div>
                         <div id="newsArticles">
-
                         </div>
                     </div>
                 </div>
