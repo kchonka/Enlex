07629322c6459a9c8365cbe6a0aaa9a408351056
Kat Chonka <katchonka@gmail.com>
2021-06-04 23:03:28 -0400

    Add use of Axios Library for API requests

--- front-end/src/learn/Learn.js
+++ front-end/src/learn/Learn.js
@@ -4,40 +4,48 @@ import SideBar from './SideBar';
 import { Dropdown } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css'; //for the Dropdown component
 import './learn.css';
+import axios from 'axios';
 
 const languageOptions = [
-    { key: 'Arabic', text: 'Arabic', value: 'Arabic' },
-    { key: 'Chinese', text: 'Chinese', value: 'Chinese' },
-    { key: 'Danish', text: 'Danish', value: 'Danish' },
-    { key: 'Dutch', text: 'Dutch', value: 'Dutch' },
-    { key: 'English', text: 'English', value: 'English' },
-    { key: 'French', text: 'French', value: 'French' },
-    { key: 'German', text: 'German', value: 'German' },
-    { key: 'Greek', text: 'Greek', value: 'Greek' },
-    { key: 'Hungarian', text: 'Hungarian', value: 'Hungarian' },
-    { key: 'Italian', text: 'Italian', value: 'Italian' }
+    { key: 'Arabic', text: 'Arabic', value: 'ar' },
+    { key: 'English', text: 'English', value: 'en' },
+    { key: 'French', text: 'French', value: 'fr' },
+    { key: 'German', text: 'German', value: 'de' },
+    { key: 'Italian', text: 'Italian', value: 'it' }
   ]
 
 export class Learn extends React.Component{
     constructor(props){
         super(props);
-        this.state = { targetLanguage: 'English' }  // default to English
+        this.state = { 
+            targetLanguage: 'en',   // default: English (en)
+            isLoaded: false
+        }
         this.handleLanguageChange = this.handleLanguageChange.bind(this);
         this.getArticles = this.getArticles.bind(this);
     }
 
     handleLanguageChange = (e, { value }) => { this.setState({ targetLanguage: value }); }
 
-    getArticles(){
-        const url = "https://newsapi.org/v2/top-headlines&apiKey=" + process.env.REACT_APP_NEWS_API_KEY;
-        fetch(url)
-            .then(response => response.json())
-            .then(data => console.log(data["articles"][0]));
+    getArticles(lang){
+        axios.get("https://newsapi.org/v2/top-headlines", {
+           params: {
+               language: lang,
+               apiKey: process.env.REACT_APP_NEWS_API_KEY
+           }
+        })
+        .then(function (response) {
+        console.log(response.data);
+        })
     }
 
     componentDidMount() {
-        this.getArticles();
-      }
+        this.getArticles(this.state.targetLanguage);
+    }
+
+    componentDidUpdate(){
+        this.getArticles(this.state.targetLanguage);
+    }
 
     render(){
         return(
